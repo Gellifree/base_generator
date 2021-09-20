90aadba6a1260f70dc9660ada6b2666e10e92921
Kovács Norbert <norbert@pop-os.localdomain>
2021-09-20 10:00:41 +0200

    add an installer

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+# SPDX-FileCopyrightText: 2021 Kovács Norbert <mfw.kovacs.norbert@gmail.com>
+#
+
+dir="$HOME/bin"
+
+
+if [ -d "$dir" ]
+then
+	cp base_generator.sh ~/bin/base_generator.sh
+else
+	mkdir ~/bin
+	cp base_generator.sh ~/bin/base_generator.sh
+	echo "To finnish the installation, please restart."
+fi
+
