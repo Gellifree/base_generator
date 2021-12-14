0df238c10d262376566bc7b265b544d12605a142
Gellifree <forge.delfadoritan@gmail.com>
2021-12-14 13:29:16 +0100

    fix(csharp): using system added

--- base_generator.sh
+++ base_generator.sh
@@ -6,7 +6,7 @@
 
 java='public class Main {\n\tpublic static void main(String[] args) {\n\t\tSystem.out.println("Helló világ!\\n");\n\t}\n}'
 
-csharp='namespace HelloWorld\n{\n\t class Hello{\n\t \t static void Main(string[] args)\n\t \t {\n\t \t \t System.Console.WriteLine("Helló Világ!");\n\t \t }\n\t }\n}'
+csharp='using System;\n\nnamespace HelloWorld\n{\n\t class Hello{\n\t \t static void Main(string[] args)\n\t \t {\n\t \t \t Console.WriteLine("Helló Világ!");\n\t \t }\n\t }\n}'
 
 vala='public static int main(string[] args) {\n\n\tstdout.printf("Hello, World!\\n");\n\n\treturn 0;\n}'
 
